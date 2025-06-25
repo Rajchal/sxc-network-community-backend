6b23e07243eb14da2903446d098d632fa0f872f9
Rajchal <rajchalanjal1@gmail.com>
2025-06-25 21:16:36 +0545

    rm gitignore and new event

--- events/models.py
+++ events/models.py
@@ -4,6 +4,7 @@ class Event(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     date = models.DateTimeField()
+    event_type = models.CharField(max_length=100,default="Workshop")
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
